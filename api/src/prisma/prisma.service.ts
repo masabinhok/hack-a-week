f71b86b1e28a464a56a2cb592393ae93eb9a2334
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2026-01-02 16:42:04 +0545

    feat: refactor PrismaService constructor to handle database URL and password encoding

--- api/src/prisma/prisma.service.ts
+++ api/src/prisma/prisma.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'src/generated/prisma/client';
+import pg from 'pg'
 
 @Injectable()
 export class PrismaService extends PrismaClient {
     constructor(){
-        const adapter = new PrismaPg({url: process.env.DATABASE_URL!});
-        super({adapter});
+        const dbUrl = String(process.env.DATABASE_URL || '').trim();
+        const urlObj = new URL(dbUrl);
+        if (!urlObj.password) throw new Error('Password missing in DATABASE_URL');
+        urlObj.password = encodeURIComponent(String(urlObj.password));
+        const finalConnectionUrl = urlObj.href;
+        const pool = new pg.Pool({ connectionString: finalConnectionUrl });
+        const adapter = new PrismaPg(pool);
+        super({ adapter });
     }
 }
