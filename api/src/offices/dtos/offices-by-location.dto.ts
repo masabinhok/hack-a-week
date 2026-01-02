fe049bde5cbecfe4225b15e20aab63064a096529
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2026-01-02 16:58:45 +0545

    feat: add validation decorators for provinceId, districtId, municipalityId, and wardId in OfficesByLocationDto

--- api/src/offices/dtos/offices-by-location.dto.ts
+++ api/src/offices/dtos/offices-by-location.dto.ts
@@ -1,6 +1,15 @@
+import { IsNumber } from "class-validator";
+
 export class OfficesByLocationDto {
+    @IsNumber()
     provinceId: number;
+
+    @IsNumber()
     districtId: number;
+
+    @IsNumber()
     municipalityId: number;
+
+    @IsNumber()
     wardId: number;
 }
